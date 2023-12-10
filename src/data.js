b84db3cf981946109df33e39e7f0dc0fe10afc0b
https://github.com/Idan3011/new_file.git <idanrot91@gmail.com>
2023-12-10 02:31:43 +0200

    change some src

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 const foodTypes =[
-    {name:'Hamburger', img:'../../public/pics/Hamburger-logo.png'},
-    {name:'Pizza', img:'../../public/pics/pizza-logo.png'},
-    {name:'Taco', img:'../../public/pics/taco-logo.png'},
-    {name:'Steak', img:'../../public/pics/steak-logo.png'}
+    {name:'Hamburger', img:'/pics/Hamburger-logo.png'},
+    {name:'Pizza', img:'/pics/pizza-logo.png'},
+    {name:'Taco', img:'/pics/taco-logo.png'},
+    {name:'Steak', img:'/pics/steak-logo.png'}
 ]
 
 export default foodTypes
